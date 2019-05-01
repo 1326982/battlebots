51bf5b233d6221b17aa45686ad8fc45efb481ab3
antoine <1326982@cstj.qc.ca>
2019-04-30 22:54:27 -0400

    1.1.1-beta
    Player Browsing system functionnal,

---
+++ Assets/CellInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellInfo : MonoBehaviour
+{
+    [SerializeField] Text txtLevel;
+    [SerializeField] Text txtRanking;
+    [SerializeField] Text txtUsername;
+    [SerializeField] GameObject btnBattle;
+    [SerializeField] GameObject btnAddFriend;
+    [SerializeField] GameObject btnRemoveFriend;
+    private bool mutualFriend;
+    private int friendshipID = 0;
+    private int userID; 
+
+    public void init(int level, int ranking, string username, int userid, bool mutual, int friendshipid ){
+        txtLevel.text = level.ToString();
+        txtRanking.text = "Rank "+ranking;
+        txtUsername.text = username;
+        userID = userid;
+        mutualFriend = mutual;
+        friendshipID = friendshipid;
+        buttonManaging();
+    }
+    public void buttonManaging(){
+        if(mutualFriend){
+            btnBattle.SetActive(true);
+        }
+        if(friendshipID == 0){
+            btnAddFriend.SetActive(true);
+        }else {
+            btnRemoveFriend.SetActive(true);
+        }
+    }
+    
+    public void launchBattle() {
+        
+    }
+    public void askFriendship() {
+
+    }
+    public void deleteFriendship(){
+
+    }
+}
---
+++ Assets/PlayerFormManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+using System.Text.RegularExpressions;
+
+public class PlayerFormManager : MonoBehaviour
+{
+    public static PlayerFormManager instance;
+    [SerializeField] private GameObject displayContent;
+    [SerializeField] private GameObject perfabListCell;
+    [SerializeField] private InputField champRecherche;
+
+    private DisplaySection section = DisplaySection.Friends;
+    private int browsePage=1;
+
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable() {
+        refreshDisplay();
+    }
+
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void refreshDisplay() {
+        emptyContent();
+        print("getting Friends");
+        string query = "";
+        switch(section){
+            case DisplaySection.Friends:
+                query = "&action=getFriends&usersID="+ PlayerPrefs.GetString("usersID");
+            break;
+            case DisplaySection.Browse:
+                query = "&action=getAllUsers&usersID="+ PlayerPrefs.GetString("usersID");
+            break;
+            case DisplaySection.Search:
+                query = "&action=searchUser&usersID="+ PlayerPrefs.GetString("usersID")+"&search="+MySqlEscape("%"+champRecherche.text.Replace(" ","%")+"%");
+            break;
+        }
+        StartCoroutine(DatabaseManager.instance.Query(showResults,query));
+    }
+    private void showResults(string response){
+        HttpOpponentWrapper users = JsonUtility.FromJson<HttpOpponentWrapper>(response);
+        foreach(HttpOpponent user in users.wrap){
+            GameObject cell = Instantiate(perfabListCell) as GameObject;
+            cell.GetComponent<RectTransform>().SetParent(displayContent.transform,false);
+            cell.GetComponent<CellInfo>().init(user.level,user.rank,user.username,user.id,user.mutualFriend,user.friendshipId);
+        }
+        
+    }
+    private string MySqlEscape(string usString) {
+    if (usString == null)
+    {
+        return null;
+    }
+    return Regex.Replace(usString, @"[\r\n\x00\x1a\\'""]", @"\$0");
+}
+    public void recherche(){
+        if(champRecherche.text != ""){
+            section = DisplaySection.Search;
+            refreshDisplay();
+        }
+        
+    }
+    public void browseAll(){
+        section = DisplaySection.Browse;
+        refreshDisplay();
+    }
+    public void showFriends(){
+        section = DisplaySection.Friends;
+        refreshDisplay();
+    }
+
+    private void emptyContent(){
+        var children = new List<GameObject>();
+        foreach (Transform child in displayContent.transform) children.Add(child.gameObject);
+        children.ForEach(child => Destroy(child));
+    }
+}
+public enum DisplaySection {
+    Friends, Browse, Search
+}
